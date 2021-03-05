99377be29a47c3135fb2e6a6c66ef2e2e403e5a1
Diego <diegoaldoburlando62@gmail.com>
2021-03-05 11:29:24 +0100

    Studying camera

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import {Image, StyleSheet,TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Contacts from 'expo-contacts';
+import { Gyroscope } from 'expo-sensors';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,9 +10,17 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+  
+     <Image source={{ uri: "https://static.toiimg.com/photo/72975551.cms" }} style={{ width: 380, height: 170 }} />
+
+     <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo AAA</Text>
+      </TouchableOpacity>
+     
     </View>
   );
 }
@@ -29,4 +40,22 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
+
+
+
